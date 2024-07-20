a1a200992af532f6ca70f0b3f6694825077d7542
Lucas Wang <lucas.wang@gradientsensing.com>
2024-07-19 22:50:05 -0700

    ImageStream test

--- tests/test_action.py
+++ tests/test_action.py
@@ -5,10 +5,49 @@ from xthings.xthing import XThing
 from xthings.descriptors import ActionDescriptor
 from pydantic import StrictInt
 from xthings.decorators import xthings_action
-
+import pytest
+import uuid
+import time
 
 service_type = "_http._tcp.local."
 service_name = "thing._http._tcp.local."
+
+
+class MyXThing(XThing):
+    ad = ActionDescriptor(
+        lambda xthing, x, cancellation_token, logger: x + 1,
+        input_model=StrictInt,
+        output_model=StrictInt,
+    )
+
+    @xthings_action(input_model=StrictInt, output_model=StrictInt)
+    def func(self, i: StrictInt, cancellation_token, logger) -> StrictInt:
+        return i + 1
+
+    @xthings_action(input_model=StrictInt, output_model=StrictInt)
+    def func_error(self, i: StrictInt, cancellation_token, logger) -> StrictInt:
+        raise Exception("error")
+        return i + 1
+
+    @xthings_action(input_model=StrictInt, output_model=StrictInt)
+    def func_slow(self, i: StrictInt, cancellation_token, logger) -> StrictInt:
+        for i in range(10):
+            time.sleep(0)
+            cancellation_token.check(0)
+
+        return i + 1
+
+
+server: XThingsServer
+xthing: MyXThing
+
+
+@pytest.fixture(autouse=True)
+def setup():
+    global server, xthing
+    server = XThingsServer()
+    xthing = MyXThing(service_type, service_name)
+    server.add_xthing(xthing, "/xthing")
 
 
 def test_action_initialization():
@@ -31,26 +70,6 @@ def test_action_initialization():
 
 
 def test_action_add_to_app():
-    class MyXThing(XThing):
-        ad = ActionDescriptor(
-            lambda xthing, x, cancellation_token, logger: x + 1,
-            input_model=StrictInt,
-            output_model=StrictInt,
-        )
-
-        @xthings_action(input_model=StrictInt, output_model=StrictInt)
-        def func(self, i: StrictInt, cancellation_token, logger) -> StrictInt:
-            return i + 1
-
-        @xthings_action(input_model=StrictInt, output_model=StrictInt)
-        def func_error(self, i: StrictInt, cancellation_token, logger) -> StrictInt:
-            raise Exception("error")
-            return i + 1
-
-    server = XThingsServer()
-    xthing = MyXThing(service_type, service_name)
-    server.add_xthing(xthing, "/xthing")
-
     with TestClient(server.app) as client:
         r = client.post("/xthing/ad", json=1)
         assert r.status_code == 201
@@ -60,6 +79,27 @@ def test_action_add_to_app():
 
         r = client.get("/invocations")
         assert r.json()[0]["status"] == "completed"
+
+        invocation_id = r.json()[0]["id"]
+        r = client.get(f"/invocations/{invocation_id}")
+        assert r.json()["status"] == "completed"
+
+        r = client.delete(f"/invocations/{invocation_id}")
+        assert r.status_code == 200
+
+        invocation_id = str(uuid.uuid4())
+        r = client.get(f"/invocations/{invocation_id}")
+        assert r.status_code == 404
+
+        r = client.post("/xthing/func_slow", json=1)
+        assert r.status_code == 201
+        r = client.get("/xthing/func_slow")
+        assert len(r.json()) == 1
+        invocation_id = r.json()[0]["id"]
+        r = client.delete(f"/invocations/{invocation_id}")
+        assert r.status_code == 200
+        # r = client.get(f"/invocations/{invocation_id}")
+        # assert r.json()["status"] == "completed"
 
         r = client.post("/xthing/func", json=1)
         assert r.status_code == 201
@@ -80,4 +120,4 @@ def test_action_add_to_app():
         r = client.get(
             "/invocations"
         )  # Fixme: get twice to wait for next task in the event loop
-        assert r.json()[2]["status"] == "error"
+        assert r.json()[3]["status"] == "error"
---
+++ tests/test_stream.py
@@ -0,0 +1,83 @@
+from fastapi.testclient import TestClient
+
+from xthings.server import XThingsServer
+from xthings.xthing import XThing
+from xthings.descriptors import ActionDescriptor, PngImageStreamDescriptor
+from pydantic import StrictInt
+from xthings.decorators import xthings_action
+import pytest
+import time
+import numpy as np
+
+
+service_type = "_http._tcp.local."
+service_name = "thing._http._tcp.local."
+
+
+class MyXThing(XThing):
+    ad = ActionDescriptor(
+        lambda xthing, x, cancellation_token, logger: x + 1,
+        input_model=StrictInt,
+        output_model=StrictInt,
+    )
+    png_stream_cv = PngImageStreamDescriptor(ringbuffer_size=100)
+
+    @xthings_action(input_model=StrictInt, output_model=StrictInt)
+    def func(self, i: StrictInt, cancellation_token, logger) -> StrictInt:
+        return i + 1
+
+    @xthings_action(input_model=StrictInt, output_model=StrictInt)
+    def func_error(self, i: StrictInt, cancellation_token, logger) -> StrictInt:
+        raise Exception("error")
+        return i + 1
+
+    @xthings_action(input_model=StrictInt, output_model=StrictInt)
+    def func_slow(self, i: StrictInt, cancellation_token, logger) -> StrictInt:
+        for i in range(10):
+            time.sleep(0)
+            cancellation_token.check(0)
+
+        return i + 1
+
+    @xthings_action(input_model=StrictInt, output_model=StrictInt)
+    def start_png_stream_cv(self, s: StrictInt, cancellatioin_token, logger):
+        if not self._streaming:
+            self._streaming = True
+            while self._streaming:
+                frame = np.random.rand(480, 640, 3) * 255
+                frame = frame.astype(np.uint8)
+
+                if self.png_stream_cv.add_frame(
+                    frame=frame, portal=self._xthings_blocking_portal
+                ):
+                    self.last_frame_index = self.png_stream_cv.last_frame_i
+
+    @xthings_action(input_model=StrictInt, output_model=StrictInt)
+    def stop_png_stream_cv(self, s: StrictInt, cancellation_token, logger):
+        print(self.png_stream_cv)
+        self._streaming = False
+
+
+server: XThingsServer
+xthing: MyXThing
+
+
+@pytest.fixture(autouse=True)
+def setup():
+    global server, xthing
+    server = XThingsServer()
+    xthing = MyXThing(service_type, service_name)
+    server.add_xthing(xthing, "/xthing")
+
+
+def test_action_add_to_app():
+    with TestClient(server.app) as client:
+        r = client.post("/xthing/start_png_stream_cv", json=1)
+        assert r.status_code == 201
+        frame = np.random.rand(480, 640, 3) * 255
+        frame = frame.astype(np.uint8)
+        xthing.png_stream_cv.add_frame(
+            frame=frame, portal=xthing._xthings_blocking_portal
+        )
+
+        xthing.png_stream_cv.stop()
